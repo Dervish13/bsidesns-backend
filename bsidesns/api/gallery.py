8947a853f4cbe58e1061d36fd1838d0182943c17
Goran Mekić <meka@tilda.center>
2020-03-10 14:47:03 +0100

    Upload permissions

--- bsidesns/api/gallery.py
+++ bsidesns/api/gallery.py
@@ -3,12 +3,14 @@ import shutil
 
 from flask import current_app
 from flask.views import MethodView
+from flask_jwt_extended import get_jwt_identity, jwt_optional
 from flask_smorest import Blueprint, abort
 from freenit.schemas.paging import PageInSchema, paginate
 from werkzeug.utils import secure_filename
 
 from ..models.event import Event
 from ..models.gallery import GalleryAlbum, GalleryFile
+from ..models.user import User
 from ..schemas.gallery import (
     GalleryAlbumPageOutSchema,
     GalleryAlbumSchema,
@@ -63,6 +65,7 @@ class GalleryAlbumAPI(MethodView):
         album.prefix = prefix
         return album
 
+    @jwt_optional
     @blueprint.arguments(GalleryUploadSchema, location='files')
     @blueprint.arguments(ResumableGalleryUploadSchema, location='form')
     @blueprint.response(ResumableGalleryUploadSchema)
@@ -79,8 +82,17 @@ class GalleryAlbumAPI(MethodView):
             album = GalleryAlbum.get(name=name, event=event)
         except GalleryAlbum.DoesNotExist:
             abort(404, message='No such album')
-        album.prefix = current_app.config.get('MEDIA_URL', None)
-        if album.prefix is None:
+        user_id = get_jwt_identity()
+        if user_id is None:
+            abort(401, message='Must be logged in to upload!')
+        try:
+            user = User.get(id=user_id)
+        except User.DoesNotExist:
+            abort(404, message='No such user')
+        if not user.admin and album.name != 'avatars':
+            abort(403, message='Only avatar uploads are allowed!')
+        prefix = current_app.config.get('MEDIA_URL', None)
+        if prefix is None:
             abort(409, message='Backend misconfiguration, no MEDIAL_URL')
         uploadFile = fileargs['file']
         chunkNumber = formargs['resumableChunkNumber']
@@ -91,7 +103,7 @@ class GalleryAlbumAPI(MethodView):
             current_app.config.get(
                 'MEDIA_PATH',
                 None,
-            )
+            ),
         )
         filePath = f'/tmp/{identifier}'
         with open(filePath, 'ab') as tempfile:
@@ -99,15 +111,23 @@ class GalleryAlbumAPI(MethodView):
             tempfile.seek(offset)
             tempfile.write(uploadFile.read())
         if chunkNumber == total:
-            try:
+            if album.name == 'avatars':
+                category = formargs['resumableType'][:5]
+                if category != 'image':
+                    abort(409, message='Only image type allowed')
+                imgtype = formargs['resumableType'][6:]
+                filename = f'{user.id}.{imgtype}'
+            else:
                 filename = secure_filename(uploadFile.filename)
-                formargs['resumableFilename'] = filename
+            try:
                 finalFile = GalleryFile.get(album=album, filename=filename)
             except GalleryFile.DoesNotExist:
                 finalFile = GalleryFile(
                     album=album,
-                    filename=secure_filename(uploadFile.filename),
+                    filename=secure_filename(filename),
                 )
+            print(finalFile.filename)
+            formargs['resumableFilename'] = finalFile.filename
             if event is None:
                 file_dir = f'{media_path}/{album.name}'
             else:
@@ -118,4 +138,7 @@ class GalleryAlbumAPI(MethodView):
             shutil.move(filePath, finalPath)
             os.chmod(finalPath, 0o644)
             finalFile.save()
+            if album.name == 'avatars':
+                user.avatar = finalFile.url(prefix=prefix)
+                user.save()
         return formargs
